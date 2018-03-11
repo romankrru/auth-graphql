b6ffbddde9639bb88d2f24ea9aaeaa3d098cce03
Roman <rm07ru@gmail.com>
2018-03-11 14:17:10 +0300

    Add login mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -33,9 +33,26 @@ const mutation = new GraphQLObjectType({
 
       resolve(parentValue, args, req) {
         const { user } = req;
-        
+
         req.logout();
         return user;
+      },
+    },
+
+    login: {
+      type: UserType,
+
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({
+          email,
+          password,
+          req,
+        });
       },
     },
   },
