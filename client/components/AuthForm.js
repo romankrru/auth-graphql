3345cb58248c4ab4c225fd48a689c5a460ada0ba
Roman <rm07ru@gmail.com>
2018-03-17 19:23:17 +0300

    Add onSubmit handler

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -10,10 +10,19 @@ class AuthForm extends Component {
     };
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    
+    this.props.onSubmit({
+      email: this.state.email,
+      password: this.state.password,
+    });
+  }
+
   render() {
     return (
       <div className="row">
-        <form className="col s4">
+        <form className="col s4" onSubmit={this.onSubmit.bind(this)}>
           <div className="input-field">
             <label htmlFor="email" className="active">Email</label>
 
@@ -30,6 +39,7 @@ class AuthForm extends Component {
             <input
               id="password"
               value={this.state.password}
+              type="password"
               onChange={e => this.setState({ password: e.target.value })}
             />
           </div>
